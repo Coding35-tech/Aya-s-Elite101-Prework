daccf032d1382badc34212562020e49318b5c3f1
Coding35-tech <aya.elmoussaoui317@gmail.com>
2025-09-17 00:06:25 +0000

    Edited intro to make it smoother(in terms of asking input and delivering sentences to user)

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print('Welcome to the Pretty in Pink Boutique catalog Chatbot')
-name = input('What is your name?')
-age = input('Hello ' + name  + 'How old are you? ')
+name = input('What is your name? ')
+age = input('Hello ' + name + ', how old are you? ')
